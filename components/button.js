66925bfdeb43e5d86bbfcfedbe18cbd5841745f1
morvaygergo <morvaygergo@gmail.com>
2019-01-16 21:05:58 +0100

    func: add button

---
+++ components/button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const button = ({ clicked, value}) => <button onClick={clicked}>{value}</button>;
+
+button.propTypes = {
+  clicked: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+}
+
+export default button;
