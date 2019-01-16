ba7bbbaa913d8f12c4c9e3e4c4db329faf98d9a8
morvaygergo <morvaygergo@gmail.com>
2019-01-16 21:12:08 +0100

    refactor: refactor button

--- components/button.js
+++ components/button.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const button = ({ clicked, value}) => <button onClick={clicked}>{value}</button>;
