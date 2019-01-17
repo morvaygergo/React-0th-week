2d18c4fa40b6732c2c88a4fea09fa6943c6db3da
morvaygergo <morvaygergo@gmail.com>
2019-01-17 16:46:13 +0100

    chore: update App

--- components/App.js
+++ components/App.js
@@ -1,46 +1,14 @@
-import React, { Component } from "react";
-import Button from "./Button";
-import Display from "./Display";
+import React from 'react';
+import BuyAcorn from './containers/BuyAcorn';
+import EatAcorn from './containers/EatAcorn';
+import DisplayAcorn from './containers/DisplayAcorn';
 
-class App extends Component {
-  state = {
-    counter: 0
-  };
-
-  componentDidMount() {
-    document.addEventListener("keydown", e => {
-      if (e.keyCode === 38) {
-        this.incrementCounter();
-      } else if (e.keyCode === 40) {
-        this.decreaseCounter();
-      }
-    });
-  }
-
-  incrementCounter = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  };
-
-  decreaseCounter = () => {
-    const { counter } = this.state;
-    if (counter > 0) {
-      this.setState({ counter: counter - 1 });
-    }
-  };
-
-  render() {
-    const { counter } = this.state;
-    return (
-      <div>
-        <Button clicked={this.incrementCounter} value="Buy one" />
-        <Display>
-          <h1>{counter} 🌰</h1>
-        </Display>
-        <Button clicked={this.decreaseCounter} value="Eat one" />
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <main>
+    <BuyAcorn />
+    <DisplayAcorn />
+    <EatAcorn />
+  </main>
+);
 
 export default App;
