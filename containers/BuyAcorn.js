f5296f2ce5ba4e8417488e3ba7b30be68094f760
morvaygergo <morvaygergo@gmail.com>
2019-01-17 16:58:12 +0100

    chore: create containers

---
+++ containers/BuyAcorn.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { buyAcorn } from '../actions/buyAcorn';
+import Button from '../../components/Button';
+
+const mapStateToProps = (state) => {
+  return {
+    value: 'Buy one',
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClick: () => {
+      dispatch(buyAcorn(1));
+    },
+  };
+};
+
+const BuyAcorn = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Button);
+
+export default BuyAcorn;
