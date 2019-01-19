f2aac449dcb15325879c617f688357479fbb7a87
morvaygergo <morvaygergo@gmail.com>
2019-01-19 20:20:01 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from './App';
 import Store from './Store';
 import { Provider } from 'react-redux';
 
